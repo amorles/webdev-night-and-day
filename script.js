28d08c4410a9590751159ccc25d644daa8ea75cb
Alejandro Morles <20232@columbushs.com>
2019-10-29 09:30:22 -0400

    fixed a bit more

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
   
 $(document).ready(function() {
-    $('button').click(function(){
+    $('#orb').click(function(){
         if ($('body').hasClass('black')) {
             $('body').removeClass('black').addClass('white');
         }
         else {
             $('body').removeClass('white').addClass('black');
+        }    
+            if ($(this).hasClass('sun')) {
+                $(this).removeClass('sun').addClass('moon');
+            }
+            else {
+                $(this).removeClass('moon').addClass('sun');
+            }
+        if ($('#sky').hasClass('day')) {
+            $('#sky').removeClass('day').addClass('night');
+        }
+        else {
+            $('#sky').removeClass('night').addClass('day');
         }
     });
 });
